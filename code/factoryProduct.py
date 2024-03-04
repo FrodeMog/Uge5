e82cda92b8e20084906d27578ccac3f5fe17be07
Frederik Mogensen <frode.moge@gmail.com>
2024-03-04 12:09:19 +0100

    add white background

--- code/factoryProduct.py
+++ code/factoryProduct.py
@@ -1,4 +1,6 @@
 from productDetails import ProductDetails
+from userDetails import UserDetails
+from transactionDetails import TransactionDetails
 
 class FactoryProduct:
     def __init__(self):
@@ -14,3 +16,27 @@ class FactoryProduct:
         product = ProductDetails(product_id, manufacturer_id, manufacturer, name, price, currency, **kwargs)
 
         return product
+
+class FactoryUser:
+    def __init__(self):
+        pass
+
+    def create_user(self, user_id, name, age, email, address, shipping_address, phone, loginDetails):
+        if not all([user_id, name, age, email, address, shipping_address, phone, loginDetails]):
+            raise ValueError("Mandatory fields cannot be empty")
+
+        user = UserDetails(user_id, name, age, email, address, shipping_address, phone, loginDetails)
+
+        return user
+
+class FactoryTransaction:
+    def __init__(self):
+        pass
+
+    def create_transaction(self, transactionId, userDetails, Product, cardDetails):
+        if not all([transactionId, userDetails, Product, cardDetails]):
+            raise ValueError("Mandatory fields cannot be empty")
+
+        transaction = TransactionDetails(transactionId, userDetails, Product, cardDetails)
+
+        return transaction
