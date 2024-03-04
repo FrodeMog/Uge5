1e6fd5d293b84940197287cb9c08d52172b64d8d
Frederik Mogensen <frode.moge@gmail.com>
2024-03-04 12:00:47 +0100

    add logindetails to user

---
+++ code/loginDetails.py
@@ -0,0 +1,5 @@
+class LoginDetails:
+    #Do some password hashing / encryption here
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
--- code/userDetails.py
+++ code/userDetails.py
@@ -1,15 +1,21 @@
+from loginDetails import LoginDetails
+
 class UserDetails:
     def __init__(self,
+                user_id,
                 name,
                 age,
                 email,
                 address,
                 shipping_address,
-                phone):
+                phone,
+                loginDetails: LoginDetails):
         
+        self.user_id = user_id
         self.name = name
         self.age = age
         self.email = email
         self.address = address
         self.shipping_address = shipping_address
         self.phone = phone
+        self.loginDetails = loginDetails
