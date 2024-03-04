1f868e68ec4fd398b3a0f3f9f76d0e4cda17ecad
Frederik Mogensen <frode.moge@gmail.com>
2024-03-04 10:51:05 +0100

    add card details to transaction

---
+++ code/cardDetails.py
@@ -0,0 +1,11 @@
+class CardDetails:
+    def __init__(self, 
+                 cardNumber, 
+                 cardHolderName, 
+                 expiryDate, 
+                 cvv):
+        
+        self.cardNumber = cardNumber
+        self.cardHolderName = cardHolderName
+        self.expiryDate = expiryDate
+        self.cvv = cvv
--- code/transactionDetails.py
+++ code/transactionDetails.py
@@ -1,12 +1,15 @@
 import product as product
 import userDetails as userDetails
+import cardDetails as cardDetails
 
 class TransactionDetails:
     def __init__(self, 
                  transactionId, 
                  userDetails: userDetails.UserDetails, 
-                 Product: product.Product):
+                 Product: product.Product,
+                 cardDetails: cardDetails.CardDetails):
         
         self.userDetails = userDetails
         self.transactionId = transactionId
         self.Product = Product
+        self.cardDetails = cardDetails
