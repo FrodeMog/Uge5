0c6ff940f0f09ba6cdebfe635940a026f02849f2
Frederik Mogensen <frode.moge@gmail.com>
2024-03-06 12:19:07 +0100

    factory for card

---
+++ code/factoryCard.py
@@ -0,0 +1,16 @@
+from details.cardDetails import CardDetails
+from factory import Factory
+
+class FactoryCard(Factory):
+    def __init__(self):
+        pass
+
+    def create_card(self, card_data=None, **kwargs):
+        if card_data is None:
+            card_data = kwargs
+        else:
+            card_data.update(kwargs)
+
+        card = super().create(CardDetails, card_data)
+
+        return card
