773cd4c857cf29f769ab47dc9401e1e3b3475b88
FrodeMog <96060147+FrodeMog@users.noreply.github.com>
2024-03-07 11:54:06 +0100

    weird check for login class inside user test

--- code/newTest.py
+++ code/newTest.py
@@ -82,7 +82,7 @@ class TestFactory(unittest.TestCase):
         user = userFactory.create(self.user_data)
         for key, value in self.user_data.items():
             if key == "login":
-                self.assertEqual(type(getattr(user, key)), value)
+                self.assertEqual(getattr(user, key), value.__class__)
             else:
                 self.assertEqual(getattr(user, key), value)
     
